24fc3d231abd2da3acb75c05e1ad1bbb1c2a3ef5
DLR <Bronnikovingvar@gmail.com>
2025-04-26 18:28:11 +0400

    260425

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Test : MonoBehaviour
 {
+    int s = 0;
+    Test d;
+    BoxCollider gh;
+    public Rigidbody rb;
+    public Transform pos;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 dir = new Vector3(0, 0, 0);
         
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
-}
+        // Update is called once per frame
+        void Update()
+        {
+        Camera.main.transform.position = pos.position - new Vector3(-10,-10,0);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(new Vector3(0, 100, 0));
+
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.AddForce(new Vector3(-10, 0, 0));
+
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.AddForce(new Vector3(10, 0, 0));
+
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.AddForce(new Vector3(0, 0, -10));
+
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.AddForce(new Vector3(0, 0, 10));
+
+            }
+
+
+        }
+    }
